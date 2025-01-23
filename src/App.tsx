a4265a0ef741c4c6383b857dedd1a3070aeeaf43
Raaby Omar <raabyo04@gmail.com>
2025-01-22 21:52:29 -0500

    fix app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import React from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +6,17 @@ function App() {
   return (
     <>
       <h1>Welcome to Web Dev</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
